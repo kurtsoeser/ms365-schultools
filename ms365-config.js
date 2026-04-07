9df052b71d228ddcbcb36428a69448a026f7d345
kurtsoeser <kurt@kurtsoeser.at>
2026-04-07 18:00:57 +0200

    Client ID eingetragen

--- ms365-config.js
+++ ms365-config.js
@@ -3,7 +3,7 @@
  * Ausführliche Schritte: siehe ms365-config.example.js (Kommentarblock oben).
  */
 window.MS365_MSAL_CONFIG = {
-    clientId: '',
+    clientId: 'e1d877c3-004c-4040-8c3b-81a59e0c7050',
     authority: 'https://login.microsoftonline.com/organizations',
     redirectUri: typeof window !== 'undefined' ? window.location.href.split('#')[0] : ''
 };
