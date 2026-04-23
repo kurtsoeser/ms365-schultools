458c0c3fd0af959f8983a1ed335cc4097486cbe4
kurtsoeser <kurt@kurtsoeser.at>
2026-04-23 17:54:07 +0200

    MS365-Gruppen-Seite

--- vite.config.js
+++ vite.config.js
@@ -28,7 +28,12 @@ export default defineConfig(() => {
           teamsArchiv: resolve(__dirname, 'tools/teams-archiv.html'),
           gruppenUebersicht: resolve(__dirname, 'tools/gruppen-uebersicht.html'),
           klassenUmbenennen: resolve(__dirname, 'tools/klassen-umbenennen.html'),
-          schuelerLehrerGruppen: resolve(__dirname, 'tools/schueler-lehrer-gruppen.html')
+          schuelerLehrerGruppen: resolve(__dirname, 'tools/schueler-lehrer-gruppen.html'),
+          weitereTeamsGruppen: resolve(__dirname, 'tools/weitere-teams-gruppen.html'),
+          weitereTeamsGruppenBesitzer: resolve(__dirname, 'tools/weitere-teams-gruppen-2-besitzer.html'),
+          weitereTeamsGruppenMitglieder: resolve(__dirname, 'tools/weitere-teams-gruppen-3-mitglieder.html'),
+          weitereTeamsGruppenEinstellungen: resolve(__dirname, 'tools/weitere-teams-gruppen-4-einstellungen.html'),
+          weitereTeamsGruppenAusfuehren: resolve(__dirname, 'tools/weitere-teams-gruppen-5-ausfuehren.html')
         }
       }
     }
